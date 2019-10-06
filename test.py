050e6bc0d51b1ee0febe941b9307c94a8b475c97
Escargot32 <56220596+Escargot32@users.noreply.github.com>
2019-10-06 20:50:00 +0900

    Add files via upload

---
+++ test.py
@@ -0,0 +1,47 @@
+from flask import Flask, request, abort
+
+from linebot import (
+    LineBotApi, WebhookHandler
+)
+from linebot.exceptions import (
+    InvalidSignatureError
+)
+from linebot.models import (
+    MessageEvent, TextMessage, TextSendMessage,
+)
+import os
+
+app = Flask(__name__)
+
+ACCESS_TOKEN = os.environ["YOUR_CHANNEL_ACCESS_TOKEN"]
+CHANNEL_SECRET = os.environ["YOUR_CHANNEL_SECRET"]
+
+line_bot_api = LineBotApi(ACCESS_TOKEN)
+handler = WebhookHandler(CHANNEL_SECRET)
+
+@app.route("/callback", methods=['post'])
+def callback():
+    signature = request.headers['X-Line=Signature']
+
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+
+    return 'OK'
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=event.message.text))
+
+
+if __name__ == "__main__":
+
+    port = int(os.getenv(("PORT", 5000)))
+    app.run("0.0.0.0", port=port)
+
